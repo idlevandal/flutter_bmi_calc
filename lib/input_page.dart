230a2e2992f248a89544a6f813048248907f9624
David Allen <davidallen@Davids-iMac.local>
2021-01-26 16:08:30 +1000

    initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,92 @@
+import 'package:bmi_calculator/reusable_card.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
+const bottomContainerColour = Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+
+  Gender selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: ReusableCard(
+                        onPress: () {
+                          setState(() {
+                            selectedGender = Gender.male;
+                          });
+                        },
+                        colour: selectedGender == Gender.male ? activeCardColour : inactiveCardColour,
+                        cardChild: IconContent('MALE', FontAwesomeIcons.mars),
+                      )
+                  ),
+                  Expanded(
+                      child: ReusableCard(
+                        onPress: () {
+                          setState(() {
+                            selectedGender = Gender.female;
+                          });
+                        },
+                        colour: selectedGender == Gender.female ? activeCardColour : inactiveCardColour,
+                        cardChild: IconContent('FEMALE', FontAwesomeIcons.venus),
+                      )
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: ReusableCard(
+                  colour: activeCardColour,
+                )
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: ReusableCard(
+                        colour: activeCardColour,
+                      )
+                  ),
+                  Expanded(
+                      child: ReusableCard(
+                        colour: activeCardColour,
+                      )
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: bottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
+          ],
+        ));
+  }
+}
+
